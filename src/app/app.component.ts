e14ce2fcbf8849edb505e2596de86abb73e7cae7
Lamia114 <lamiaibri2011@gmail.com>
2023-09-19 21:33:27 +0400

    dd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ToDoList';
+  title = 'ToDoList App';
+  list:any[]=[];
+  addTask(item:string)
+  {
+    this.list.push({id:this.list.length,name:item})
+    console.warn(this.list);
+  }
+  removeTask(id:number)
+  {
+    console.warn(id)
+    this.list=this.list.filter(item=>item.id!==id);
+  }
+
 }
